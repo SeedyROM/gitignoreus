ac57b134d05fe49344df097a7f9d42dc3f97353b
Zack Kollar <rallokkcaz@gmail.com>
2018-10-19 06:15:58 -0700

    More ES6 refactors

--- src/server.js
+++ src/server.js
@@ -11,21 +11,20 @@ const {
 
 // Check default ignores
 asyncGlob('./defaults/**/*.gitignore')
-  .then((files) => {
+  .then((files) =>
     files.forEach((location) => {
       const filePath = path.resolve(__dirname, '../', location)
-      console.log(filePath)
       const data = fs.readFileSync(filePath, 'utf8')
       console.log(data)
     })
-  })
+  )
 
 const host = (process.env.NODE_ENV === 'prod') ? '0.0.0.0' : 'localhost'
 const port = process.env.PORT || 8000
 
 const server = http.createServer(app)
-server.listen(port, host, null, () => {
+server.listen(port, host, null, () =>
   log.server(`Listening ${host}:${port}...`)
-})
+)
 
 module.exports = server
