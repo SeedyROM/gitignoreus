1916fbee4bd6ae31980e5d9ed55cd07ce72defd0
Zack Kollar <rallokkcaz@gmail.com>
2018-10-19 05:48:52 -0700

    Added database connection exception

--- src/database/index.js
+++ src/database/index.js
@@ -1,24 +1,30 @@
 const mongoose = require('mongoose')
-
+const findOrCreate = require('mongoose-findorcreate')
 const log = require('../dev/log')
 const {
   getDatabaseURI
 } = require('./helpers')
 
+// Inject plug-ins
+mongoose.plugin(findOrCreate)
+
+// Setup exports
 module.exports = app => {
+  // Construct database URI
   const uri = getDatabaseURI()
   mongoose.connect(uri, {
     useNewUrlParser: true,
     useCreateIndex: true
   })
 
+  //
   const database = mongoose.connection
-
   database.on('open', () => {
     log.database(`Database connection established...`)
   })
-  database.on('error', () => {
-    log.database(`MongoDB connection error:`)
+  database.on('error', (err) => {
+    log.database(`Database connection failed!`)
+    throw err
   })
 
   app.database = database
